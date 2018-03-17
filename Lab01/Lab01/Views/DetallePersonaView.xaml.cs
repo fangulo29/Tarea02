86e0384dc8ce8f517b3e085e726168232b13e068
Fernando Angulo <nandoandresag@gmail.com>
2018-03-17 01:57:16 -0600

    Tarea02

--- Lab01/Lab01/Views/DetallePersonaView.xaml.cs
+++ Lab01/Lab01/Views/DetallePersonaView.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Lab01.ViewModels;
+﻿using Lab01.Models;
+using Lab01.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +15,11 @@ namespace Lab01.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DetallePersonaView : ContentPage
 	{
-		public DetallePersonaView ()
+        public ObservableCollection<Person> vloListaPersonas { get; set; }
+
+        public DetallePersonaView ()
 		{
 			InitializeComponent ();
-
             BindingContext = new PersonViewModel();
         }
 	}
